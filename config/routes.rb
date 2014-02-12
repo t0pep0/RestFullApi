bc5d4529085042b5738030407cee31f153eb7597
t0pep0 <t0pep0.gentoo@gmail.com>
2014-02-12 10:02:34 +0400

    Fix bugs on update\create

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get '/api/doc/:major/:model' => 'api#description'
   get '/api/:major/:model' => 'api#index'
   post '/api/:major/:model' => 'api#create'
-  get '/api/:major/:model/new' => 'api#new'
-  get '/api/:major/:model/:id/edit' => 'api#edit'
   get '/api/:major/:model/:id' => 'api#show'
   put '/api/:major/:model/:id' => 'api#update'
   delete '/api/:major/:model/:id' => 'api#destroy'
