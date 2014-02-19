047f5414744f7d08d38e17e9c2d181facb325a02
t0pep0 <t0pep0.gentoo@gmail.com>
2014-02-19 11:41:15 +0400

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get '/api/:major/:model/:id/:edge' => 'api#edge'
   get '/api/doc/:major/:model' => 'api#description'
   get '/api/:major/:model' => 'api#index'
   post '/api/:major/:model' => 'api#create'
   get '/api/:major/:model/:id' => 'api#show'
   put '/api/:major/:model/:id' => 'api#update'
   delete '/api/:major/:model/:id' => 'api#destroy'
-  get '/api/:major/:model/:id/:edge' => 'api#edge'
 end
