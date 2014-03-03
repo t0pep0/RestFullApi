ed5c41715a8aaab7169bf4e50d2ac232143fbd44
t0pep0 <t0pep0.gentoo@gmail.com>
2014-03-03 10:44:31 +0400

    embed fix

--- lib/RestFullApi/api.rb
+++ lib/RestFullApi/api.rb
@@ -226,7 +226,7 @@ class RestFullApi::Api < ActionController::Base
         embed_obj = record.send(embed)
 				embed_model = (embed_obj.instance_of?(Array) ? embed_obj.new.class.model_name.to_s : embed_obj.class.model_name.to_s)
 	embed_obj_attr = @version_config[:options][:attributes_accessible][embed_model.to_sym]
-        subembed = embed_obj_attr if subembed.nil?
+        subembed = embed_obj_attr if subembed = [nil]
 
 	if embed_obj.instance_of? Array
 		embed_obj.each do |obj|
